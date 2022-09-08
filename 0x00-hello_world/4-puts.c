2af0b7ef7ae8e1d744360af803a00960ce158330
Ugwu-Code <anthonychibuzomario@gmail.com>
2022-09-08 12:57:14 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <stdio>
 
 /**
- * main - prints the phrase "programming is like building a multingual puzzle
+ * main	- prints the phrase "programming is like building a multilingual puzzle
  * Return: 0 if exited properly, non-zero otherwise
  */
-int main(void)
-{
-        puts("\"programming is like building a multilingual puzzle");
-        return	(0);
-} 	
+ int main(void)
+ {
+	 puts("\"programming is like building a multiligual puzzle");
+	 return	(0):
+ }
