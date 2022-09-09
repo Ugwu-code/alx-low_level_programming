46a59efde5162f027f9ef9a2aa852fd78ed8400f
Ugwu-Code <anthonychibuzomario@gmail.com>
2022-09-09 02:56:15 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main	- prints the size of varius types
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	printf("Size of a char: %byte(s)\n", sizeof(char));
+	printf("Size of an int: %byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
