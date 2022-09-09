59b6a2ade14dd05b9a502ab592f3125c0486fa76
Ugwu-Code <anthonychibuzomario@gmail.com>
2022-09-09 03:32:50 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main	- prints the size of varius types
+ * main	- prints the size of various types
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
