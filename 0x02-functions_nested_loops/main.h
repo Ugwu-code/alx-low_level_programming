1a2b28dd98e09d3c2829078a45e5cfc7f2d50433
Ugwu-Code <anthonychibuzomario@gmail.com>
2022-09-14 05:32:08 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * main -  prints main, followed by a new line
- * Return: Always 0 (Success)
+ * main -  print putchar
+ * Return: Always 0
  */
+
 int main(void)
 {
 	_putchar('_');
