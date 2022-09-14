e8e978585dd90ad5bb60a9630c5110eeba010331
Ugwu-Code <anthonychibuzomario@gmail.com>
2022-09-13 23:55:27 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,25 @@
+Write a program that prints _putchar, followed by a new line.
+
+Write a function that prints the alphabet, in lowercase, followed by a new line.
+
+Write a function that prints 10 times the alphabet, in lowercase, followed by a new line.
+
+Write a function that checks for lowercase character.
+
+Write a function that checks for alphabetic character.
+
+Write a function that prints the sign of a number.
+
+Write a function that computes the absolute value of an integer.
+
+Write a function that prints the last digit of a number.
+
+Write a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+
+Write a function that prints the 9 times table, starting with 0.
+
+Write a function that adds two integers and returns the result.
+
+Write a function that prints all natural numbers from n to 98, followed by a new line.
+
+
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+int main(void);
+void print_alphabet(void);
+void print_alphabet_x10(void;
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+int 101-natural.c(int n);
+int 102-fibonacci.c(int 1, int 2);
+int 103-fibonacci.c(int n);
+int _putchar(char c);
