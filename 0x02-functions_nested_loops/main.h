08013167a6fdd2aac0d108faf3420d078c26ec70
Ugwu-Code <anthonychibuzomario@gmail.com>
2022-09-14 05:21:58 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,22 +1,20 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
+/**
+ * main -  prints main, followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
-void print_alphabet(void);
-void print_alphabet_x10(void;
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
-int 101-natural.c(int n);
-int 102-fibonacci.c(int 1, int 2);
-int 103-fibonacci.c(int n);
-int _putchar(char c);
-
-#endif
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('n');
+	
+	return (0);
+}
