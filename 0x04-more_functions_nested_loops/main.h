ed46fdb0a26d238abcc5f37ed7023fd75025123c
Ugwu-Code <anthonychibuzomario@gmail.com>
2022-09-15 10:10:46 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - header file for protoypes
+ * void protoypes (void)
+ * int protoypes (int)
+ */
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+int _putchar(char);
+int mul(int, int);
+
+#endif /* MAIN_H */
